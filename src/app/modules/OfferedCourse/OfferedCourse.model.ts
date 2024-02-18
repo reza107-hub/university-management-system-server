b0fd689cd94f6149e1a9188d5bb6bb11e47c3036
Antor-094 <mdantor556677889@gmail.com>
2024-02-18 11:23:07 +0600

    Offred course model added

---
+++ src/app/modules/OfferedCourse/OfferedCourse.model.ts
@@ -0,0 +1,68 @@
+import mongoose, { Schema } from 'mongoose';
+import { Days } from './OfferedCourse.constant';
+import { TOfferedCourse } from './OfferedCourse.interface';
+
+const offeredCourseSchema = new mongoose.Schema<TOfferedCourse>(
+  {
+    semesterRegistrationId: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: 'SemesterRegistration',
+    },
+    academicSemesterId: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: 'AcademicSemester',
+    },
+    programId: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: 'program',
+    },
+    departmentId: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: 'department',
+    },
+    courseId: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: 'Course',
+    },
+    facultyId: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: 'faculty',
+    },
+    maxCapacity: {
+      type: Number,
+      required: true,
+    },
+    section: {
+      type: Number,
+      required: true,
+    },
+    days: [
+      {
+        type: String,
+        enum: Days,
+      },
+    ],
+    startTime: {
+      type: String,
+      required: true,
+    },
+    endTime: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export const OfferedCourse = mongoose.model<TOfferedCourse>(
+  'OfferedCourse',
+  offeredCourseSchema,
+);
